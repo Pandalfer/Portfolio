9b7c8395fcaeae34f3dc5bcd41b7d43972f435b1
Shashish <shashishpanda1@gmail.com>
2026-01-04 20:42:43 +0000

    Update technologies in TechnologiesBlackHole component
    Removed NPM, Flask, and Jinja technologies; added Construct 3 technology.

--- src/components/Technologies/TechnologiesBlackHole.tsx
+++ src/components/Technologies/TechnologiesBlackHole.tsx
@@ -10,7 +10,6 @@ export default function TechnologiesBlackHole() {
         <Center />
 
         <Ring width={400} offset={25} speed={15}>
-          <Technology src="https://cdn.simpleicons.org/react/8A2BE2" alt="React" />
           <Technology src="https://cdn.simpleicons.org/next.js/8A2BE2" alt="Next Js" />
           <Technology src="https://cdn.simpleicons.org/typescript/8A2BE2" alt="TypeScript" />
           <Technology src="https://cdn.simpleicons.org/tailwindcss/8A2BE2" alt="TailwindCSS" />
@@ -18,10 +17,8 @@ export default function TechnologiesBlackHole() {
 
         <Ring width={250} offset={0} speed={7}>
           <Technology src="https://cdn.simpleicons.org/unity/8A2BE2" alt="Unity" />
-          <Technology src="https://cdn.simpleicons.org/npm/8A2BE2" alt="NPM" />
+          <Technology src="https://cdn.simpleicons.org/construct3/8A2BE2" alt="Construct 3" />
           <Technology src="https://cdn.simpleicons.org/python/8A2BE2" alt="Python" />
-          <Technology src="https://cdn.simpleicons.org/flask/8A2BE2" alt="Flask" />
-          <Technology src="https://cdn.simpleicons.org/jinja/8A2BE2" alt="Jinja" />
         </Ring>
       </div>
     </div>
